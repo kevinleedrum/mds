0dcf0d315fc6e246e373749bace9fbd58235d1b1
JP Given <jgiven@gmail.com>
2021-04-09 10:17:16 -0700

    Copy files is working like a charm.

--- vuepress/.vuepress/config.js
+++ vuepress/.vuepress/config.js
@@ -10,6 +10,8 @@ module.exports = {
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
     ['link', { rel: 'stylesheet', href: '/styles/mds-core.css' }],
     ['script', { src: 'https://unpkg.com/phosphor-icons' }],
+    ['script', { type: 'module', src: '/components/mds-components.esm.js' }],
+    ['script', { nomodule: '', src: '/components/mds-components.js' }],
   ],
   theme: 'yuu',
   markdown: {
