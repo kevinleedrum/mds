5a28a2552b0a851ffc34eba721aedf7b607cc303
JP Given <jgiven@gmail.com>
2021-04-09 09:46:40 -0700

    Tailwind css is getting added to vuepress.

---
+++ vuepress/.vuepress/config.js
@@ -0,0 +1,18 @@
+const process = require('process');
+const { description } = require('../../package');
+
+module.exports = {
+  title: 'Moxi Design System',
+  description: description,
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'stylesheet', href: '/styles/mds-core.css' }],
+    ['script', { src: 'https://unpkg.com/phosphor-icons' }],
+  ],
+  theme: 'yuu',
+  markdown: {
+    lineNumbers: true,
+  },
+};
