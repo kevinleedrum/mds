63136497beb5f175e9844c167b1b768288bbbb5c
JP Given <jgiven@gmail.com>
2021-04-21 11:26:18 -0700

    Prepping for build process.

---
+++ build-all.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+version=`jq -r '.version' package.json`
+
+echo ""
+echo "==========================="
+echo "Building version ${version}"
+echo "==========================="
+echo ""
+
+yarn docs:build
+rm -rf ./docs
+mv public/.vuepress/dist docs
