1e05a208f3efb5772b54ac9b7e33fd27dd4b8964
JP Given <jgiven@gmail.com>
2021-04-21 14:20:29 -0700

    Pushing the dist and loader dirs to github.

---
+++ dist/types/components/mx-button/mx-button.d.ts
@@ -0,0 +1,13 @@
+export declare class MxInput {
+  btnElem: HTMLButtonElement;
+  anchorElem: HTMLAnchorElement;
+  type: string;
+  value: string;
+  disabled: boolean;
+  xl: boolean;
+  href: string;
+  target: string;
+  ripple(e: any): void;
+  returnBaseClass(): string;
+  render(): any;
+}
