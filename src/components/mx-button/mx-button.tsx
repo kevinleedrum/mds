8fd5aafe451cf4064eb08968de9fa5b95efd6f08
Kevin Lee Drum <kevinleedrum@gmail.com>
2021-05-19 21:27:10 -0400

    Fix pressed/ripple colors and ripple position

--- src/components/mx-button/mx-button.tsx
+++ src/components/mx-button/mx-button.tsx
@@ -33,6 +33,9 @@ export class MxButton {
   ripple(e: MouseEvent) {
     const elem = this.href ? this.anchorElem : this.btnElem;
 
+    let existingRipple = elem.querySelector('.ripple');
+    if (existingRipple) existingRipple.remove();
+
     // Create span element
     let ripple = document.createElement('span');
 
@@ -40,11 +43,19 @@ export class MxButton {
     ripple.classList.add('ripple');
 
     // Add span to the button
-    elem.appendChild(ripple);
+    elem.prepend(ripple);
+
+    // Set the size of the span element
+    const diameter = Math.max(elem.clientWidth, elem.clientHeight);
+    ripple.style.width = ripple.style.height = diameter + 'px';
 
     // Position the span element
-    ripple.style.left = (e.offsetX || 0) + 'px';
-    ripple.style.top = (e.offsetY || 0) + 'px';
+    const elemOffset = elem.getBoundingClientRect();
+    // Center over click coords OR over top left corner if activated by keypress
+    const left = Math.max(e.clientX - elemOffset.left, 0);
+    const top = Math.max(e.clientY - elemOffset.top, 0);
+    ripple.style.left = left - diameter / 2 + 'px';
+    ripple.style.top = top - diameter / 2 + 'px';
 
     // Remove span after 0.3s
     setTimeout(() => {
