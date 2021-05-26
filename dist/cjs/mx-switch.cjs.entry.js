45b11a52801260d94c691057f372b7b424e99e8d
JP Given <jgiven@gmail.com>
2021-05-26 09:02:07 -0700

    Build 0.0.26

--- dist/cjs/mx-switch.cjs.entry.js
+++ dist/cjs/mx-switch.cjs.entry.js
@@ -13,7 +13,7 @@ const MxSwitch = class {
     this.checked = false;
   }
   render() {
-    return (index.h(index.Host, { class: "mx-switch" }, index.h("label", { class: "relative inline-flex flex-nowrap align-center items-center cursor-pointer text-sm w-36 h-14" }, index.h("input", { class: "absolute h-0 w-0 opacity-0", type: "checkbox", name: this.name, checked: this.checked }), index.h("span", { class: "slider round" }), index.h("div", { class: "ml-48 inline-block whitespace-nowrap", "data-testid": "labelName" }, this.labelName))));
+    return (index.h(index.Host, { class: "mx-switch" }, index.h("label", { class: "relative inline-flex flex-nowrap align-center items-center cursor-pointer text-sm w-36 h-14" }, index.h("input", { class: "absolute h-0 w-0 opacity-0", role: "switch", type: "checkbox", name: this.name, checked: this.checked }), index.h("span", { class: "slider round" }), index.h("div", { class: "pl-48 inline-block whitespace-nowrap", "data-testid": "labelName" }, this.labelName))));
   }
 };
 
