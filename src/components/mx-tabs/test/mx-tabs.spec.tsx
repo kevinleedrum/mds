926e30f2793d7da74738715852f3963190eacc0c
Kevin Lee Drum <kevinleedrum@gmail.com>
2021-06-01 09:48:17 -0400

    Add tab tests

--- src/components/mx-tabs/test/mx-tabs.spec.tsx
+++ src/components/mx-tabs/test/mx-tabs.spec.tsx
@@ -1,18 +1,43 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { MxTabs } from '../mx-tabs';
+import { MxTab } from '../../mx-tab/mx-tab';
 
-describe('mx-tabs', () => {
-  it('renders', async () => {
-    const page = await newSpecPage({
-      components: [MxTabs],
-      html: `<mx-tabs></mx-tabs>`,
+describe('mx-tabs (horizontal stack)', () => {
+  let page;
+  let root: HTMLMxTabsElement;
+  beforeEach(async () => {
+    page = await newSpecPage({
+      components: [MxTabs, MxTab],
+      html: `<mx-tabs>
+      <mx-tab label="Home" />
+      <mx-tab selected label="Search" />
+      </mx-tabs>`,
     });
-    expect(page.root).toEqualHtml(`
-      <mx-tabs>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </mx-tabs>
-    `);
+    root = page.root;
+  });
+
+  it('renders an inline grid by default', async () => {
+    const grid = root.firstElementChild;
+    expect(grid.getAttribute('class')).toContain('inline-grid');
+  });
+});
+
+describe('mx-tabs (horizontal fill)', () => {
+  let page;
+  let root: HTMLMxTabsElement;
+  beforeEach(async () => {
+    page = await newSpecPage({
+      components: [MxTabs, MxTab],
+      html: `<mx-tabs fill>
+      <mx-tab label="Home" />
+      <mx-tab selected label="Search" />
+      </mx-tabs>`,
+    });
+    root = page.root;
+  });
+
+  it('renders a grid with the fill prop', async () => {
+    const grid = root.firstElementChild;
+    expect(grid.getAttribute('class')).toContain('grid');
   });
 });
