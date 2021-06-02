9fa5e7d6f8867586f768a6d130a9b5a04bfe5443
Kevin Lee Drum <kevinleedrum@gmail.com>
2021-06-02 09:51:10 -0400

    Add mx-tab-content unit tests

--- src/components/mx-tab-content/test/mx-tab-content.spec.tsx
+++ src/components/mx-tab-content/test/mx-tab-content.spec.tsx
@@ -2,17 +2,27 @@ import { newSpecPage } from '@stencil/core/testing';
 import { MxTabContent } from '../mx-tab-content';
 
 describe('mx-tab-content', () => {
-  it('renders', async () => {
-    const page = await newSpecPage({
+  let page;
+  let contents: NodeListOf<HTMLMxTabContentElement>;
+  beforeEach(async () => {
+    page = await newSpecPage({
       components: [MxTabContent],
-      html: `<mx-tab-content></mx-tab-content>`,
+      html: `
+      <mx-tab-content index="0">First</mx-tab-content>
+      <mx-tab-content index="1">Second</mx-tab-content>
+      `,
     });
-    expect(page.root).toEqualHtml(`
-      <mx-tab-content>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </mx-tab-content>
-    `);
+    contents = page.body.querySelectorAll('mx-tab-content');
+  });
+
+  it('does not show any tab content when value is not set', async () => {
+    expect(contents[0].getAttribute('class')).toContain('hidden');
+    expect(contents[1].getAttribute('class')).toContain('hidden');
+  });
+  it('displays content when the value matches its index', async () => {
+    contents.forEach(content => (content.value = 1));
+    await page.waitForChanges();
+    expect(contents[0].getAttribute('class')).toContain('hidden');
+    expect(contents[1].getAttribute('class')).not.toContain('hidden');
   });
 });
