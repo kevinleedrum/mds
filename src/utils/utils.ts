e40965052254e69d954105007ac0c216033375b8
Kevin Lee Drum <kevinleedrum@gmail.com>
2021-06-04 09:27:39 -0400

    Honor prefers-reduced-motion

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,8 @@
 export function format(first: string, middle: string, last: string): string {
   return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
 }
+
+export function queryPrefersReducedMotion(): boolean {
+  const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+  return !mediaQuery || mediaQuery.matches;
+}
