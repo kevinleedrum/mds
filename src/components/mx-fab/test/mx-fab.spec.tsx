6b84b63b7b8e326606bf9059f7f6a79c52b76690
Kevin Lee Drum <kevinleedrum@gmail.com>
2021-06-18 14:52:16 -0400

    Add mx-fab tests

---
+++ src/components/mx-fab/test/mx-fab.spec.tsx
@@ -0,0 +1,51 @@
+import '../../../utils/matchMedia.mock';
+import { newSpecPage } from '@stencil/core/testing';
+import { MxFab } from '../mx-fab';
+
+describe('mx-fab', () => {
+  let page;
+  let root: HTMLMxFabElement;
+  let button: HTMLElement;
+  beforeEach(async () => {
+    page = await newSpecPage({
+      components: [MxFab],
+      html: `<mx-fab icon="ph-heart" value="liked" aria-label="Like" />`,
+    });
+    root = page.root;
+    button = root.querySelector('button');
+  });
+
+  it('renders a button', async () => {
+    expect(button).not.toBeNull();
+  });
+
+  it('adds a secondary class if the secondary prop is set', async () => {
+    expect(button.getAttribute('class')).not.toContain('secondary');
+    root.secondary = true;
+    await page.waitForChanges();
+    expect(button.getAttribute('class')).toContain('secondary');
+  });
+
+  it('applies the value and aria-label props to the button attributes', async () => {
+    expect(button.getAttribute('value')).toBe('liked');
+    expect(button.getAttribute('aria-label')).toBe('Like');
+  });
+});
+
+describe('mx-fab (extended)', () => {
+  let page;
+  let root: HTMLMxFabElement;
+  let button: HTMLElement;
+  beforeEach(async () => {
+    page = await newSpecPage({
+      components: [MxFab],
+      html: `<mx-fab>Add</mx-fab>`,
+    });
+    root = page.root;
+    button = root.querySelector('button');
+  });
+
+  it('renders the slot content as the button label', async () => {
+    expect(button.innerText).toBe('Add');
+  });
+});
