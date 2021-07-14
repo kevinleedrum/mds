e7923f5e48b9a2bea534bd5fb0cdda688ae90e1b
Kevin Lee Drum <kevinleedrum@gmail.com>
2021-07-14 14:12:29 -0400

    Change jest detection to avoid exceptions

--- src/utils/transitions.ts
+++ src/utils/transitions.ts
@@ -51,7 +51,7 @@ function executeTransition(
   transformOrigin?: string,
 ): Promise<void> {
   return new Promise(async resolve => {
-    if (queryPrefersReducedMotion() || process.env.JEST_WORKER_ID) return resolve();
+    if (queryPrefersReducedMotion() || typeof jest !== 'undefined') return resolve();
     // Set the start value for each property
     transitionOptions.forEach(transition => {
       setStyleProperty(el, transition.property, transition.startValue);
