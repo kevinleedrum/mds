b0a07d1d179d79610404ff0f7c5da6d07914e82c
Kevin Lee Drum <kevinleedrum@gmail.com>
2021-08-03 10:18:30 -0400

    Add disabled and indeterminate states to checkbox

--- src/components/mx-checkbox/mx-checkbox.tsx
+++ src/components/mx-checkbox/mx-checkbox.tsx
@@ -11,6 +11,8 @@ export class MxCheckbox {
   @Prop() labelName: string = '';
   @Prop() labelClass: string = '';
   @Prop() checked: boolean = false;
+  @Prop() disabled: boolean = false;
+  @Prop() indeterminate: boolean = false;
 
   render() {
     return (
@@ -22,14 +24,18 @@ export class MxCheckbox {
           ].join(' ')}
         >
           <input
-            class="absolute h-0 w-0 opacity-0"
+            class={'absolute h-0 w-0 opacity-0' + (this.indeterminate ? ' indeterminate' : '')}
             type="checkbox"
             name={this.name}
             value={this.value}
             checked={this.checked}
+            disabled={this.disabled}
           />
           <span class={'flex h-18 w-18 cursor-pointer' + (this.labelLeft ? ' order-2 ml-16' : ' order-1')}></span>
-          <div class={'inline-block' + (this.labelLeft ? ' order-1 flex-1' : ' order-2 ml-16')} data-testid="labelName">
+          <div
+            class={'checkbox-label inline-block' + (this.labelLeft ? ' order-1 flex-1' : ' order-2 ml-16')}
+            data-testid="labelName"
+          >
             {this.labelName}
           </div>
         </label>
