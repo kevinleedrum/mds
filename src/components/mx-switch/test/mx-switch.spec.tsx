7d1561d4c839f01cb832402005bb3b9f6ac57dbb
Kevin Lee Drum <kevinleedrum@gmail.com>
2021-10-22 09:37:03 -0400

    Add value prop to mx-switch unit tests

--- src/components/mx-switch/test/mx-switch.spec.tsx
+++ src/components/mx-switch/test/mx-switch.spec.tsx
@@ -8,7 +8,7 @@ describe('mx-checkbox', () => {
   beforeEach(async () => {
     page = await newSpecPage({
       components: [MxSwitch],
-      html: `<mx-switch name="foo" label-name="Premier" checked="true" data-test="test" />`,
+      html: `<mx-switch name="foo" label-name="Premier" value="dog" checked="true" data-test="test" />`,
     });
     root = page.root;
     input = root.querySelector('input');
@@ -18,9 +18,10 @@ describe('mx-checkbox', () => {
     expect(input.type).toBe('checkbox');
   });
 
-  it('has the proper name and label', async () => {
+  it('has the proper name, value, and label', async () => {
     const labelName = root.querySelector('[data-testid="labelName"]');
     expect(input.getAttribute('name')).toBe('foo');
+    expect(input.getAttribute('value')).toBe('dog');
     expect(labelName.innerText).toBe('Premier');
   });
 
