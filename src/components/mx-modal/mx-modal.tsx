3dd7fe3a42d16ff0eead26e4155fc6f9f5aabaca
Kevin Lee Drum <kevinleedrum@gmail.com>
2021-11-29 11:25:52 -0500

    Remove old comments

--- src/components/mx-modal/mx-modal.tsx
+++ src/components/mx-modal/mx-modal.tsx
@@ -170,14 +170,14 @@ export class MxModal {
   get openTransition(): Function {
     let transition: Function = (el: HTMLElement) => fadeScaleIn(el, 250);
     if (this.fromRight) transition = fadeSlideIn;
-    else if (this.fromLeft) transition = (el: HTMLElement) => fadeSlideIn(el, undefined, Direction.left); // Change fromRight/toRight to fromLeft/toLeft
+    else if (this.fromLeft) transition = (el: HTMLElement) => fadeSlideIn(el, undefined, Direction.left);
     return transition;
   }
 
   get closeTransition(): Function {
     let transition: Function = fadeOut;
     if (this.fromRight) transition = fadeSlideOut;
-    else if (this.fromLeft) transition = (el: HTMLElement) => fadeSlideOut(el, undefined, Direction.left); // Change fromRight/toRight to fromLeft/toLeft
+    else if (this.fromLeft) transition = (el: HTMLElement) => fadeSlideOut(el, undefined, Direction.left);
     return transition;
   }
 
