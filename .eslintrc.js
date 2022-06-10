5b7ed45af4c5d7a0ab5e5ad7967af0ef27ee6a83
Kevin Lee Drum <kevinleedrum@gmail.com>
2022-06-10 11:06:50 -0400

    Fix eslint warnings in js files & stencil config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,25 +4,28 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended'],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    'prefer-const': 'off',
-  },
+  ignorePatterns: ['stencil.config.ts'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
+      extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+      parser: '@typescript-eslint/parser',
       parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
         project: ['./tsconfig.json'],
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
       },
     },
   ],
