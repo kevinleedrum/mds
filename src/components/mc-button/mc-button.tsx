414f46d849fae99a0eec3602b8b1fd1915bf19d3
Kevin Lee Drum <kevinleedrum@gmail.com>
2022-09-14 10:00:10 -0400

    Move prop comments to interface

--- src/components/mc-button/mc-button.tsx
+++ src/components/mc-button/mc-button.tsx
@@ -9,16 +9,24 @@ export type ButtonTypeAttribute = 'button' | 'submit' | 'reset';
 export interface IMcButtonProps {
   btnType?: McBtnType;
   disabled?: boolean;
+  /** Show dropdown icon on right (shorthand for `icon-right="mds-caret-down-fill"`) */
   dropdown?: boolean;
+  /** The aria-label attribute for the inner button element. */
   elAriaLabel?: string;
   form?: string;
   formaction?: string;
+  /** Sets display to flex instead of inline-flex */
   full?: boolean;
+  /** Create button as link */
   href?: string;
+  /** Sets the min-width to 0.  When combined with a single icon/slot, the text is not centered in the button. */
   hug?: boolean;
+  /** Class name of left icon */
   iconLeft?: string;
+  /** Class name of right icon */
   iconRight?: string;
   small?: boolean; // TODO: Remove if "small" ends up being a unique type.
+  /** Only for link buttons */
   target?: string;
   type?: ButtonTypeAttribute;
   value?: string;
@@ -35,24 +43,16 @@ export class McButton implements IMcButtonProps {
 
   @Prop({ mutable: true }) btnType: McBtnType = 'normal';
   @Prop() disabled = false;
-  /** Show dropdown icon on right (shorthand for `icon-right="mds-caret-down-fill"`) */
   @Prop() dropdown = false;
-  /** The aria-label attribute for the inner button element. */
   @Prop() elAriaLabel: string;
   @Prop() formaction: string;
   @Prop() form: string;
-  /** Sets display to flex instead of inline-flex */
   @Prop() full = false;
-  /** Create button as link */
   @Prop() href: string;
-  /** Sets the min-width to 0.  When combined with a single icon/slot, the text is not centered in the button. */
   @Prop() hug = false;
-  /** Class name of left icon */
   @Prop() iconLeft: string;
-  /** Class name of right icon */
   @Prop() iconRight: string;
   @Prop() small = false; // TODO: Remove if "small" ends up being a unique type.
-  /** Only for link buttons */
   @Prop() target: string;
   @Prop() type: ButtonTypeAttribute = 'button';
   @Prop() value: string;
@@ -81,7 +81,7 @@ export class McButton implements IMcButtonProps {
     let str = this.btnType + '-button'; // Sets color vars
     str += ' flex items-center justify-center relative overflow-hidden appearance-none select-none';
     str += ' cursor-pointer disabled:pointer-events-none disabled:cursor-auto hover:no-underline';
-    str += ' w-full rounded font-bold caption1 uppercase';
+    str += ' w-full rounded font-bold caption1 uppercase'; // TODO: Replace caption1 class if necesary
     if (this.btnType === 'ghost') str += ' border';
     // TODO: Update below lines if "small" ends up being a unique type.
     str += this.small ? ' min-h-30' : ' min-h-40';
