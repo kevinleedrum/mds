26cdeb4f7352ed78d98f2bed563fed89805be117
Mitchell Sutton <mitchell.sutton@moxiworks.com>
2023-06-15 16:17:52 +1000

    Moved packages

---
+++ packages/stencil-library/stencil.config.ts
@@ -0,0 +1,40 @@
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+import { reactOutputTarget } from '@stencil/react-output-target';
+import { inlineSvg } from 'stencil-inline-svg';
+
+export const config: Config = {
+  namespace: 'mds-components',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    reactOutputTarget({
+      componentCorePackage: '../..',
+      proxiesFile: './react/src/components.ts',
+      includeDefineCustomElements: true,
+    }),
+    {
+      type: 'dist-custom-elements-bundle',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+    {
+      type: 'dist-hydrate-script',
+      dir: '../hydrate'
+    },
+  ],
+  plugins: [sass(), inlineSvg()],
+  testing: {
+    transform: {
+      // Stub in assets that jest does not know how to import
+      '^.+\\.svg$': '<rootDir>/jestAssetTransform.js',
+    },
+  },
+};
