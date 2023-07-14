2a60179b57fc5da3aa236613dd773607ad88fcb6
Mitchell Sutton <mitchell.sutton@moxiworks.com>
2023-07-14 11:13:40 +1000

    For now making it no different

--- packages/mds/stencil.config.react.ts
+++ packages/mds/stencil.config.react.ts
@@ -1,11 +1,15 @@
 import { Config } from '@stencil/core';
-import { config as baseConfig, dist, react } from './stencil.config'
+import { config as baseConfig, dist, distCustomElements, hydrate, react } from './stencil.config'
 
 // Only builds the outputs requireed for react
-// React needs the dist for the loader export
+// React needs the dist for the loader export, and hydrate for the build
 export const config: Config = {
     ...baseConfig,
   outputTargets: [
+    // This will be required until we change the package.json from
+    // "module": "dist/custom-elements/index.js", to "./dist/index.js"
+    distCustomElements,
+    hydrate,
     dist,
     react,
   ],
