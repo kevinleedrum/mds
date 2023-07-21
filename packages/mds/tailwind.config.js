bb73c06b17586f3c46df4791f06f4033131d3531
John McClumpha <1267217+johnmcclumpha@users.noreply.github.com>
2023-07-21 12:00:41 +1000

    fix: update default border-radius

--- packages/mds/tailwind.config.js
+++ packages/mds/tailwind.config.js
@@ -37,6 +37,15 @@ const config = {
       '1-25': '0.078rem',
       '1-5': '0.094rem',
     },
+    borderRadius: {
+      'none': '0',
+      'sm': '0.125rem',
+      DEFAULT: '0.19rem',
+      'md': '0.375rem',
+      'lg': '0.5rem',
+      'full': '9999px',
+      'large': '12px',
+    },    
     spacing: {
       0: '0px',
       1: '1px',
